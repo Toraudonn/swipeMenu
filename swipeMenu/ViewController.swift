70a608031beec97a0558bc22043c9e234b5c5da5
haruyaishikawa <toraudonn@gmail.com>
2018-05-12 00:49:45 +0900

    Initial Commit

---
+++ swipeMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swipeMenu
+//
+//  Created by Haruya Ishikawa on 2018/05/12.
+//  Copyright © 2018 Haruya Ishikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
